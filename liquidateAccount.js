29c22cf630e6cce3e9a39c0eb032b5af0e2a37e4
YameenMalik <yameen@seed.im>
2022-09-29 21:03:07 +0500

    minor corrections

--- liquidateAccount.js
+++ liquidateAccount.js
@@ -1,13 +1,13 @@
-const { Perpetual__factory } = require("@firefly-exchange/library");
+const { Perpetual__factory } = require("@firefly-exchange/library/dist/src/contracts/exchange");
 const { ethers, toBigNumberStr, Trader } = require("@firefly-exchange/library");
-const { FireflyClient, Networks } = require("@firefly-exchange/firefly-client");
 const environment = require("./environments.json");
 
 require('dotenv').config();
-env = environment[process.env.env]
+envName = process.env.env;
+env = environment[envName]
 market = process.env.market;
 
-console.log("Env:", process.env.env);
+console.log("Env:", envName);
 console.log("Market:", market);
 
 
@@ -17,36 +17,23 @@ const perpetualContractAddress = env[market]["Perpetual"]
 
 const provider = new ethers.providers.JsonRpcProvider(providerURL);
 
-const network = env == "TESTNET" ? Networks.TESTNET : Networks.DEV
-
-async function main(liquidatePvtKey, liquidatorPvtKey, leverage, quantity){
-
-    const client = new FireflyClient(true, network, liquidatePvtKey);
-    client.init()
+async function main(liquidatorAddress, liquidatorPvtKey, leverage, quantity, positionSide){
 
     const liquidatorWallet = new ethers.Wallet(liquidatorPvtKey, provider);
     const contract = Perpetual__factory.connect(perpetualContractAddress, liquidatorWallet);
-
-    const position = await client.getUserPosition({symbol:pairName});
-    
-    if(!position.ok){
-        console.error(`Could not find user ${client.getPublicAddress()} position info on DAPI`);
-        process.exit(1);
-    }
 
     const params = await Trader.setupLiquidationTrade(
         {
             // complete order quantity
             quantity: toBigNumberStr(quantity),
             // is buy should always be same as maker order is buy.
-            isBuy: position.data.side == "BUY",
-            // isBuy: true,
+            isBuy: positionSide == "long",
             // use default leverage as one
             leverage: toBigNumberStr(leverage),
             allOrNothing: false
         },
         liquidatorWallet.address,
-        await client.getPublicAddress(),
+        liquidatorAddress,
         liquidationContractAddress
     );
 
@@ -61,12 +48,12 @@ async function main(liquidatePvtKey, liquidatorPvtKey, leverage, quantity){
 
 
 if(require.main === module){
-    if(process.argv.length != 6){
+    if(process.argv.length != 7){
       console.error(`Provide account address to be liquidated, liquidator's private key, leverage and quantity
-      \ne.g. "yarn liquidate <liquidate pvt key> <liquidator pvt key> <leverage> <quantity>"
-      \ne.g  "yarn liquidate key1 key2 4 10
+      \ne.g. "yarn liquidate <liquidate address> <liquidator pvt key> <leverage> <quantity> <position side>"
+      \ne.g  "yarn liquidate address key2 4 10 long
       `);
       process.exit(1);  
     };
-    main(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+    main(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
 }
