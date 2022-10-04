ff2239b30ef245fd3bfca13c1f1cb9f665f92bb3
Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>
2022-10-04 18:28:14 +0500

    tests scripts for guardian methods

---
+++ performFinalSettlement.js
@@ -0,0 +1,42 @@
+const { Guardian__factory, Perpetual__factory } = require("@firefly-exchange/library/dist/src/contracts/exchange");
+const { ethers,toBigNumberStr } = require("@firefly-exchange/library");
+
+const environment = require("./environments.json");
+require('dotenv').config();
+env = environment[process.env.env]
+market = process.env.market;
+
+console.log("Env:", process.env.env);
+console.log("Market:", market);
+
+const providerURL = env["URL"];
+const owner = env["DEPLOYER"]
+const perpetualContractAddress = env[market]["Perpetual"]
+
+const provider = new ethers.providers.JsonRpcProvider(providerURL);
+//const deployerWallet = new ethers.Wallet(owner, provider);
+
+
+
+async function main(priceLowerBound,priceUpperBound,ownerPvtKey){
+    /* only run this section if final settlement is not enabled already */
+    // const contract = Perpetual__factory.connect(perpetualContractAddress, deployerWallet); 
+    // await ( await contract.enableFinalSettlement(toBigNumberStr(priceLowerBound),toBigNumberStr(priceUpperBound))).wait();
+     
+    console.log("Perform Final Settlement on:", ownerPvtKey);
+    const owner = ownerPvtKey;
+    const ownerWallet = new ethers.Wallet(owner, provider);
+    contract = Perpetual__factory.connect(perpetualContractAddress, ownerWallet); 
+    const address = await ownerWallet.getAddress();
+    console.log ("balance", +await provider.getBalance(await ownerWallet.getAddress()));
+    await ( await contract.withdrawFinalSettlement(ownerWallet.address )).wait();
+}
+
+
+if(require.main === module){
+    if(process.argv.length != 5){
+      console.error(`provide oraclePriceUpperBound,oraclePriceLowerBound and ownerPvtKey e.g. yarn performFinalSettlementStatus <ownerPvtKey> `);
+      process.exit(1);  
+    };   
+    main(process.argv[2],process.argv[3],process.argv[4]);
+}
---
+++ setFinalSettlementStatus.js
@@ -0,0 +1,33 @@
+const { Guardian__factory, Perpetual__factory } = require("@firefly-exchange/library/dist/src/contracts/exchange");
+const { ethers,toBigNumberStr } = require("@firefly-exchange/library");
+
+const environment = require("./environments.json");
+require('dotenv').config();
+env = environment[process.env.env]
+market = process.env.market;
+
+console.log("Env:", process.env.env);
+console.log("Market:", market);
+
+
+const providerURL = env["URL"];
+const owner = env["DEPLOYER"]
+const perpetualContractAddress = env[market]["Perpetual"]
+
+const provider = new ethers.providers.JsonRpcProvider(providerURL);
+const ownerWallet = new ethers.Wallet(owner, provider);
+
+async function main(priceLowerBound,priceUpperBound){
+    console.log("Set Final Settlement Enable Status:")
+    const contract = Perpetual__factory.connect(perpetualContractAddress, ownerWallet);
+    await ( await contract.enableFinalSettlement(toBigNumberStr(priceLowerBound),toBigNumberStr(priceUpperBound))).wait();
+}
+
+
+if(require.main === module){
+    if(process.argv.length != 4){
+      console.error(`provide oraclePriceUpperBound and oraclePriceLowerBound e.g. yarn setFinalSettlementStatus <oraclePriceUpperBound> <oraclePriceUpperBound>`);
+      process.exit(1);  
+    };   
+    main(process.argv[2],process.argv[3]);
+}
